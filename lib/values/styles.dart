f15b64f7eb5537bda46584b33517e746a3588dff
mr4c50 <ocalin.ocalan@gmail.com>
2020-10-30 22:15:18 -0600

    Se agrega clase de colores y estilos

---
+++ lib/values/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:iteso_app/utilities/material_color.dart';
+
+class Styles {
+  static final MaterialColor azulIteso =
+      generateMaterialColor(Color(0xFF22489d));
+  static final MaterialColor azulBajito =
+      generateMaterialColor(Color(0x0FF1875c5));
+}
