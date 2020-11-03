c9e742986497ab3d36a69b4c281bf3aa44a27449
mr4c50 <ocalin.ocalan@gmail.com>
2020-11-03 11:56:21 -0600

    Remodelacion pantalla servicios

--- lib/app/modules/servicios_iteso/views/servicios_iteso_view.dart
+++ lib/app/modules/servicios_iteso/views/servicios_iteso_view.dart
@@ -6,7 +6,7 @@ class ServiciosItesoView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Servicios ITESO'),
+          title: Text('services'.tr),
           centerTitle: true,
         ),
         body: ListView(
@@ -20,24 +20,67 @@ class ServiciosItesoView extends StatelessWidget {
 }
 
 class ItesoAppService extends StatelessWidget {
-  final String androidPackage;
-  final String iOSPackage;
-
   final String title;
-  final String photoPath;
 
-  const ItesoAppService(
-      {Key key,
-      this.androidPackage,
-      this.iOSPackage,
-      this.title,
-      this.photoPath})
-      : super(key: key);
+  const ItesoAppService({
+    Key key,
+    @required this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Text(title.tr),
+    return Container(
+      margin: EdgeInsets.all(15),
+      height: 200,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(.5),
+            blurRadius: 10.0, // soften the shadow
+            spreadRadius: 5.0, //extend the shadow
+            offset: Offset(
+              6.0, // Move to right 10  horizontally
+              6.0, // Move to bottom 10 Vertically
+            ),
+          )
+        ],
+      ),
+      child: Stack(
+        children: [
+          Container(
+            height: 200,
+            child: Opacity(
+              opacity: 0.8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  "https://iteso.mx/image/journal/article?img_id=14584551&t=1548109348352",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: (MediaQuery.of(context).size.width - 40) * 0.7,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white70,
+                ),
+                margin: EdgeInsets.only(bottom: 15, left: 15),
+                child: SizedBox(
+                  width: (MediaQuery.of(context).size.width - 40) * 0.7,
+                  child: Text(
+                    title.tr,
+                    textAlign: TextAlign.center,
+                    style: Get.textTheme.headline6.copyWith(fontSize: 17),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
